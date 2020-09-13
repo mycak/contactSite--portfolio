42bacabb63ee4d1c1338c83b073b21319b416351
mycak <mycak@wp.pl>
2020-09-13 19:51:12 +0200

    next changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,15 @@
 import React, { useRef } from 'react';
 import { connect } from 'react-redux';
 import '../styles/header.css';
-import img from '../helperFiles/images/1.png';
 
 const Header = ({isMainActive, scrollY}) => {
 
-    const titleDiv = useRef(null);
-    const subtitleDiv = useRef(null);
-    const descDiv = useRef(null);
-    const picDiv = useRef(null);
     const header = useRef(null);
     const headerBottom = header.current? header.current.offsetHeight : '';
 
     const isActiveHeader2 = isMainActive ? 'active' : '';
     const isActiveHeader = (isMainActive && scrollY < .7*headerBottom) ? 'active' : '';
-    const isActiveHeader3 = (scrollY > .7*headerBottom) ? 'roll' : '';
+    const isActiveHeader3 = (scrollY >= .7*headerBottom) ? 'roll' : '';
     
     return (
         <section className="section section--header" id="header" ref={header}>
@@ -24,18 +19,18 @@ const Header = ({isMainActive, scrollY}) => {
                 </div>
                 <div className="header--main--container">
                     <div className="header--text--container">
-                        <div className={`header--title ${isActiveHeader}`} useref={titleDiv}>
+                        <div className={`header--title ${isActiveHeader}`}>
                             <p><span className="gradient-text">Piotr Myszkiewicz</span></p>
                         </div>
-                        <div className={`header--subtitle ${isActiveHeader}`} useref={subtitleDiv}>
+                        <div className={`header--subtitle ${isActiveHeader}`}>
                             <p>Front-end Developer</p>
                         </div>
-                        <div className={`header--description ${isActiveHeader}`} useref={descDiv}>
+                        <div className={`header--description ${isActiveHeader}`}>
                             <p>Voluptate sit occaecat irure adipisicing et id deserunt qui <span className="highlighted-text">duis</span> duis velit elit velit laboris. Minim incididunt minim ullamco <span className="highlighted-text">consequat</span> nostrud consequat exercitation. Enim anim non dolor et proident ut officia.Nisi mollit aliquip nisi aliquip occaecat <span className="highlighted-text">laboris</span> deserunt</p>
                         </div>
                     </div>
-                    <div className={`header--pic--container ${isActiveHeader}`} useref={picDiv}>
-                        <img src={img} alt="portrait" />
+                    <div className={`header--pic--container ${isActiveHeader}`}>
+                        {/* <img src={img} alt="portrait" /> */}
                     </div>
                 </div>
             </header>
--- src/components/SectionProjects.js
+++ src/components/SectionProjects.js
@@ -6,50 +6,74 @@ import img2 from '../helperFiles/images/proj-2.jpeg';
 import img3 from '../helperFiles/images/proj-3.jpeg';
 import img4 from '../helperFiles/images/proj-4.jpeg';
 
-const SectionProjects = ({scrollY}) => {
+const SectionProjects = () => {
+    console.log('esa')
     const projectsContainer = useRef(null);
-    const pictureContainer = useRef(null);
+    const picturesContainer = useRef(null);
     const leftSideContainer = useRef(null);
     const leftSideTextContainer = useRef(null);
+    const changingTextContainer = useRef(null);
+    const firstPic = useRef(null);
+
+
     const projectsContainerTop = (projectsContainer.current) ? projectsContainer.current.offsetTop : '';
     const projectsContainerBottom = (projectsContainer.current) ? projectsContainer.current.offsetTop + projectsContainer.current.offsetHeight: '';
-    const isActiveSection = (scrollY > .7*(projectsContainerTop)) ? 'active' : '';
- 
+    const isActiveSection = (window.scrollY > .7*(projectsContainerTop)) ? 'active' : '';
+    
    function leftSideIn () {
-            if(leftSideContainer.current && scrollY > leftSideContainer.current.offsetTop && scrollY < projectsContainerBottom - window.innerHeight){
+            if(leftSideContainer.current && window.scrollY > leftSideContainer.current.offsetTop && window.scrollY < projectsContainerBottom - window.innerHeight){
             leftSideTextContainer.current.style.width = leftSideContainer.current.offsetWidth +'px';
             return 'active'
         } else return '';
     }
 
-    if (scrollY>400) {
-        const projectPics = [...pictureContainer.current.children]
-        projectPics.forEach(pic => {
-            if (pic.getBoundingClientRect().top){
-            console.log('')
-            }
-        })
+    const changeText = () => {
+        const text = [
+            'Pariatur tempor eiusmod velit eiusmod quis magna sit aliqua aliquip. Ullamco pariatur eiusmod aliqua ea enim reprehenderit dolor eiusmod fugiat qui ullamco exercitation duis. Cupidatat sit id culpa pariatur tempor ullamco eiusmod veniam aute incididunt. Mollit laborum dolor et velit sint sunt qui pariatur ex. Dolor elit est incididunt ullamco et do magna velit nostrud dolor sint non. Eiusmod eu enim veniam reprehenderit dolore nulla mollit nostrud deserunt. Laborum ex mollit cupidatat eu officia velit enim adipisicing aliquip.',
+            'Kielbasa zapiekanki browary',
+            'Non anim voluptate cupidatat magna Lorem cupidatat esse sint Lorem aliquip. Ipsum aliquip do nisi exercitation Lorem officia nostrud aliquip nulla reprehenderit fugiat anim ullamco Lorem. Culpa ea Lorem nulla voluptate nisi eu nulla est cupidatat quis enim eu. Occaecat sit pariatur commodo ex fugiat veniam proident aliqua pariatur commodo id. Et irure amet minim sint ad aute excepteur sunt Lorem aute veniam eu. Proident sint ullamco cillum irure ipsum est mollit. Commodo quis laboris labore aute.',
+            'essa essa essa'
+        ]
+        
+        if (window.scrollY>400) {
+            const distanseBetweenPics = firstPic.current.offsetTop - projectsContainerTop;
+            const projectPics = [...picturesContainer.current.children];
+            let isActive = false;
+            projectPics.forEach((pic) => {
+                const i = pic.dataset.id;
+                if (pic.getBoundingClientRect().top <= .5*distanseBetweenPics-100 && pic.getBoundingClientRect().top + pic.offsetHeight + .5*distanseBetweenPics -100 >=0){
+                    isActive = !isActive;
+                    console.log(pic)
+                    changingTextContainer.current.classList.remove('active');
+                    changingTextContainer.current.innerHTML = text[i];
+                    changingTextContainer.current.classList.add('active');
+                }
+            })
+        }
     }
-
+    
+    changeText();
     return (
         <section id="projects" className="section section--projects" ref={projectsContainer}>
             <div className={`projects--leftside--container ${isActiveSection}`} ref={leftSideContainer}>
                 <div className={`project--text--container ${leftSideIn()}`} ref={leftSideTextContainer}>
-                    <p>Mycha</p>
+                    <div className="changingText--container" ref={changingTextContainer}>
+                        
+                    </div>
                 </div>
             </div>
             <div className="projects--rightside--container">
-                <div className="projects--pics--container" ref={pictureContainer}>
-                    <div className="projects--pic--container">
+                <div className="projects--pics--container" ref={picturesContainer}>
+                    <div className="projects--pic--container" data-id="0" ref={firstPic}>
                         <img src={img1} alt="proj"></img>
                     </div>
-                    <div className="projects--pic--container">
+                    <div className="projects--pic--container" data-id="1">
                         <img src={img2} alt="proj"></img>
                     </div>
-                    <div className="projects--pic--container">
+                    <div className="projects--pic--container" data-id="2">
                         <img src={img3} alt="proj"></img>
                     </div>
-                    <div className="projects--pic--container">
+                    <div className="projects--pic--container" data-id="3">
                         <img src={img4} alt="proj"></img>
                     </div>
                 </div>
